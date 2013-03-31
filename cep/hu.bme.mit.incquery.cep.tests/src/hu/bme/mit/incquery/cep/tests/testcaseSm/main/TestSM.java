21c10cd613580861bb62d55fa66e1ab8beee45dd
Istvan David <goerenh@gmail.com>
2013-03-31 19:52:20 +0200

    modified testcase

--- cep/hu.bme.mit.incquery.cep.tests/src/hu/bme/mit/incquery/cep/tests/testcaseSm/main/TestSM.java
+++ cep/hu.bme.mit.incquery.cep.tests/src/hu/bme/mit/incquery/cep/tests/testcaseSm/main/TestSM.java
@@ -58,31 +58,30 @@ public class TestSM {
 	// commented code is related to the generated queries
 	@Test
 	public void test() throws InterruptedException, IncQueryException {
-		// Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-		// Map<String, Object> m = reg.getExtensionToFactoryMap();
-		// m.put("cep", new XMIResourceFactoryImpl());
-		//
-		// ResourceSet rs = new ResourceSetImpl();
-		// Resource r = rs.createResource(URI.createURI("cep/sm.model"));
-		//
-		// if (r == null) {
-		// System.out.println("null resource");
-		// }
-		// r.getContents().add(modelManager.getModel());
-		//
-		// IncQueryEngine engine =
-		// EngineManager.getInstance().getIncQueryEngine(rs);
-		// if (engine == null) {
-		// System.out.println("null engine");
-		// }
-		//
-		// IMatcherFactory mf = MatcherFactoryRegistry
-		// .getMatcherFactory("hu.bme.mit.incquery.cep.tests.testcasesmqueries.event");
-		//
-		// if (mf == null) {
-		// System.out.println("null matcher factory");
-		// }
-		// IncQueryMatcher matcher = mf.getMatcher(engine);
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("cep", new XMIResourceFactoryImpl());
+		
+		ResourceSet rs = new ResourceSetImpl();
+		Resource r = rs.createResource(URI.createURI("cep/sm.cep"));
+		
+		if (r == null) {
+			System.out.println("null resource");
+		}
+		r.getContents().add(modelManager.getModel());
+		
+		IncQueryEngine engine = EngineManager.getInstance().getIncQueryEngine(rs);
+		if (engine == null) {
+			System.out.println("null engine");
+		}
+		
+		IMatcherFactory mf = MatcherFactoryRegistry
+				.getMatcherFactory("hu.bme.mit.incquery.cep.tests.testcasesmqueries.event");
+		
+		if (mf == null) {
+			System.out.println("null matcher factory");
+		}
+		IncQueryMatcher matcher = mf.getMatcher(engine);
 		
 		System.err.println("DIAG: Test starting.\n");
 		Thread.sleep(1000l);
@@ -91,13 +90,13 @@ public class TestSM {
 		modelManager.buildStateMachine(bcPattern);
 		
 		eventQueue.push(new B(source));
-		// check(matcher);
+		check(matcher);
 		Thread.sleep(1000l);
 		eventQueue.push(new C(source));
-		// check(matcher);
+		check(matcher);
 		Thread.sleep(1000l);
 		eventQueue.push(new A(source));
-		// check(matcher);
+		check(matcher);
 		Thread.sleep(1000l);
 		
 		System.err.println("\nDIAG: Test finished.");
