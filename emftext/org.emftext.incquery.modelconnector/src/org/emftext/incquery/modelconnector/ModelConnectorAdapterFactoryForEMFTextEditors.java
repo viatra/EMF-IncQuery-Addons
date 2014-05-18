8d212ecbcbb4d6ffef4a635b8e75392126e840c6
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2014-05-18 21:45:50 +0200

    Updated codebase for latest release

--- emftext/org.emftext.incquery.modelconnector/src/org/emftext/incquery/modelconnector/ModelConnectorAdapterFactoryForEMFTextEditors.java
+++ emftext/org.emftext.incquery.modelconnector/src/org/emftext/incquery/modelconnector/ModelConnectorAdapterFactoryForEMFTextEditors.java
@@ -16,29 +16,29 @@
 package org.emftext.incquery.modelconnector;
 
 import org.eclipse.core.runtime.IAdapterFactory;
-import org.eclipse.incquery.runtime.api.IModelConnector;
+import org.eclipse.incquery.tooling.ui.queryexplorer.IModelConnector;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.emftext.access.EMFTextAccessProxy;
 import org.emftext.access.resource.IEditor;
 
 public class ModelConnectorAdapterFactoryForEMFTextEditors implements IAdapterFactory {
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public Object getAdapter(Object adaptableObject, Class adapterType) {
-		if(adapterType.equals(IModelConnector.class) && adaptableObject instanceof ITextEditor){
-			if(EMFTextAccessProxy.isAccessibleWith(adaptableObject.getClass(), IEditor.class)){
-				IEditor emftextEditor = (IEditor) EMFTextAccessProxy.get(adaptableObject, IEditor.class);
-				return new EMFTextModelConnector((ITextEditor) adaptableObject, emftextEditor);
-			}
-		}
-		return null;
-	}
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Object getAdapter(Object adaptableObject, Class adapterType) {
+        if (adapterType.equals(IModelConnector.class) && adaptableObject instanceof ITextEditor) {
+            if (EMFTextAccessProxy.isAccessibleWith(adaptableObject.getClass(), IEditor.class)) {
+                IEditor emftextEditor = (IEditor) EMFTextAccessProxy.get(adaptableObject, IEditor.class);
+                return new EMFTextModelConnector((ITextEditor) adaptableObject, emftextEditor);
+            }
+        }
+        return null;
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public Class[] getAdapterList() {
-		return new Class[] { IModelConnector.class };
-	}
+    @SuppressWarnings("rawtypes")
+    @Override
+    public Class[] getAdapterList() {
+        return new Class[] { IModelConnector.class };
+    }
 
 }
